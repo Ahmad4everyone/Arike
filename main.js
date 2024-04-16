fc4e1a8a23c446ea5207d29d7ac391b416a92184
ahmad4everyone <ahmadmut24@gmail.com>
2024-04-16 05:00:11 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+var ade = document.getElementById('para')
+var ola = document.getElementById('about')
+var ahmad = document.getElementById("duni")
+var drk = document.getElementById("ahm")
+var pp = document.getElementById("pp")
+var btn = document.getElementById("btn")
+function answer(){
+    ade.style.display = 'block', ola.style.display = 'none', ahmad.style.display = 'none', drk.style.display = 'none', btn.style.display = 'block'
+    
+}
+function back(){
+    location.reload()
+}
+function ans(){
+    pp.style.display ='block', ola.style.display = 'none', ahmad.style.display = 'none', drk.style.display = 'none',btn.style.display = 'block'
+}
